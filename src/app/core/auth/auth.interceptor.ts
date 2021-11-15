ad9bfd184d84d6166bb7f20461fbe642eaf94eae
Ahmad Miqdaad <ahmad.miqdaad@kalsym.com>
2021-11-16 00:01:06 +0800

    reload the app after it getting 401 unauthorize

--- src/app/core/auth/auth.interceptor.ts
+++ src/app/core/auth/auth.interceptor.ts
@@ -59,7 +59,7 @@ export class AuthInterceptor implements HttpInterceptor
                     this._authService.signOut();
 
                     // Reload the app
-                    // location.reload();
+                    location.reload();
                 }
 
                 return throwError(error);
