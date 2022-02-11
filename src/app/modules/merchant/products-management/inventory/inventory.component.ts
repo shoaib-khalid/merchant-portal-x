3f13d6323c04761f0ef0dd0105a0802286c06ffc
Wafi <ahmad.wafi@kalsym.com>
2022-02-11 09:38:23 +0800

    bug fixes

--- src/app/modules/merchant/products-management/inventory/inventory.component.ts
+++ src/app/modules/merchant/products-management/inventory/inventory.component.ts
@@ -490,18 +490,19 @@ export class InventoryComponent implements OnInit, AfterViewInit, OnDestroy
         }
 
         //set the details
-        this.closeDetails();
+        // this.closeDetails();
 
         // set showVariantsSection , showCombosSection to false
         this.showVariantsSection = false;
         this.showCombosSection = false;
 
-        //to 
 
         // Get the product by id
-        this._inventoryService.getProductById(productId)
+        this._inventoryService.getProductByIdApi(productId)
             .pipe(takeUntil(this._unsubscribeAll))
-            .subscribe((product) => {
+            .subscribe((response) => {
+
+                let product = response["data"];
 
                 // Set the selected product
                 this.selectedProduct = product;
@@ -805,13 +806,10 @@ export class InventoryComponent implements OnInit, AfterViewInit, OnDestroy
             // if got variants
             if (this.selectedProductForm.get('isVariants').value === true) {
                 
-                // HANDLE INVENTORY
+                // INVENTORY
                 if (this.selectedVariantCombos.length > 0){
                     for (var i = 0; i < this.selectedVariantCombos.length; i++) {
-        
-                        // // no itemCode means it is newly created, so, call POST method
-                        // if (!this.selectedVariantCombos[i].itemCode) {
-        
+                
                         // }
                         const body = {
                             // itemCode: this.selectedProduct.id + "-" + i,
@@ -831,7 +829,7 @@ export class InventoryComponent implements OnInit, AfterViewInit, OnDestroy
                     
                 }
         
-                // HANDLE VARIANT
+                // VARIANT
                 // Delete the variant from the BE
                 if (this.variantToBeDeleted.length > 0){
     
@@ -1000,6 +998,8 @@ export class InventoryComponent implements OnInit, AfterViewInit, OnDestroy
                         this._inventoryService.addProductAssets(this.selectedProduct.id, formData, (i === this.thumbnailIndex) ? { isThumbnail: true } : { isThumbnail: false })
                             .pipe(takeUntil(this._unsubscribeAll))
                             .subscribe((response)=>{
+
+                                this.selectedProductForm.get('productAssets').value[i] = response;
                                 // Mark for check
                                 this._changeDetectorRef.markForCheck();
                             });
@@ -1008,6 +1008,17 @@ export class InventoryComponent implements OnInit, AfterViewInit, OnDestroy
             }
             // Show a success message
             this.showFlashMessage('success');
+
+            // Set delay before closing the details window
+            setTimeout(() => {
+
+                // close the details window
+                this.toggleDetails(this.selectedProduct.id)
+    
+                // Mark for check
+                this._changeDetectorRef.markForCheck();
+            }, 2000);
+
         });
 
 
@@ -1035,7 +1046,7 @@ export class InventoryComponent implements OnInit, AfterViewInit, OnDestroy
                     this._changeDetectorRef.markForCheck();
                 });
             }
-            // if it is an old one, update it
+            // if it is an old one, update it (update thumbnail)
             else if (i === this.thumbnailIndex && this.selectedVariantCombos[i].newAsset == false) {
                 await this._inventoryService.updateProductAssets(this.selectedProduct.id, { isThumbnail: true , itemCode: this.selectedProduct.id + i }, this.selectedVariantCombos[i].assetId)
                 .toPromise().then(data => {
@@ -1045,28 +1056,6 @@ export class InventoryComponent implements OnInit, AfterViewInit, OnDestroy
             }
           }
 
-        // // CREATE VARIANT IMAGES
-        // for (let i = 0; i < this.selectedVariantCombos.length; i++) {
-                
-        //     // if new, create new asset
-        //     if (this.variantimages[i].new) {
-            
-        //         let formdata = new FormData();
-        //         formdata.append("file", this.variantimages[i].file);
-        //         await this._inventoryService.addProductAssets(this.selectedProduct.id, formdata, (i === this.thumbnailIndex) ? { isThumbnail: true , itemCode: this.selectedProduct.id + i } : { isThumbnail: false, itemCode: this.selectedProduct.id + i }, i)
-        //         .toPromise().then(data => {
-        //             this._changeDetectorRef.markForCheck();
-        //         });
-        //     }
-        //     // if has id, means its old one, so update it
-        //     else if (i === this.thumbnailIndex && this.variantimages[i].id) {
-        //         await this._inventoryService.updateProductAssets(this.selectedProduct.id, { isThumbnail: true , itemCode: this.selectedProduct.id + i }, this.variantimages[i].id)
-        //         .toPromise().then(data => {
-        //         // Mark for check
-        //         this._changeDetectorRef.markForCheck();
-        //     });
-        //     }
-        //   }
 
 
         //old
@@ -3150,8 +3139,6 @@ export class InventoryComponent implements OnInit, AfterViewInit, OnDestroy
     //     console.log('this.variantComboOptions', this.variantComboOptions);
     //     console.log('this.variantImagesToBeDeleted', this.variantImagesToBeDeleted);
     //     // console.log('variantimages', this.variantimages);
-    //     console.log('this.selectedProduct.productAssets', this.selectedProduct.productAssets);
-
-        
+    //     console.log('this.products$', this.products$);
     // }
 }
