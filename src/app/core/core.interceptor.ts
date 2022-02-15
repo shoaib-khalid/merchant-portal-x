11eb1b30bdf3e51dde4c98fa5b58787bea6d55de
Ahmad Miqdaad <ahmad.miqdaad@kalsym.com>
2022-02-15 11:46:28 +0800

    fix status 0 problem

--- src/app/core/core.interceptor.ts
+++ src/app/core/core.interceptor.ts
@@ -35,31 +35,34 @@ export class CoreInterceptor implements HttpInterceptor
                 // Ignore intercept for login () clients/authenticate                
                 if ( error instanceof HttpErrorResponse && !(error.status === 401 && newReq.url.indexOf("clients/authenticate") > -1)  && !(error.status === 409))
                 {
-                // Show a error message
-                
-                const confirmation = this._fuseConfirmationService.open({
-                    title  : error.error.error ? 'Error ' + error.error.error + ' (' + error.error.status + ')': 'Error',
-                    message: error.error.message ? error.error.message : error.message,
-                    icon: {
-                        show: true,
-                        name: "heroicons_outline:exclamation",
-                        color: "warn"
-                    },
-                    actions: {
-                        confirm: {
-                            label: 'Ok',
-                            color: "primary",
+                    // Show a error message
+                    
+                    const confirmation = this._fuseConfirmationService.open({
+                        title  : error.error.error ? 'Error ' + error.error.error + ' (' + error.error.status + ')': 'Error',
+                        message: error.error.message ? error.error.message : error.message,
+                        icon: {
+                            show: true,
+                            name: "heroicons_outline:exclamation",
+                            color: "warn"
                         },
-                        cancel: {
-                            show: false,
-                        },
-                    }
-                });
+                        actions: {
+                            confirm: {
+                                label: 'Ok',
+                                color: "primary",
+                            },
+                            cancel: {
+                                show: false,
+                            },
+                        }
+                    });
 
-                    // Reload the app
-                    // location.reload();
                 }
-
+                
+                if ( error instanceof HttpErrorResponse && error.status === 0){
+                    // Reload the app
+                    location.reload();
+                }
+                
                 return throwError(error);
             })
         );
