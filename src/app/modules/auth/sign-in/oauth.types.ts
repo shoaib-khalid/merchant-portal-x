a85e3732e08ae05837dff8ce4b55f581e3ed68f9
nur.iman <nur.iman@kalsym.com>
2022-03-28 11:40:10 +0800

    Change fb client id

--- src/app/modules/auth/sign-in/oauth.types.ts
+++ src/app/modules/auth/sign-in/oauth.types.ts
@@ -12,7 +12,7 @@ export class ValidateOauthRequest{
 
 export enum SocialLooginClientId{
     GOOGLE_CLIENT_ID =  '905470792335-krfa0c32ajed37ktrn16ggj3km0783rd.apps.googleusercontent.com',
-    FACEBOOK_CLIENT_ID =  '283489330438468',                  // 'clientId'   // '283489330438468'      // '2915126152079198'
+    FACEBOOK_CLIENT_ID =  '2915126152079198',                  // 'clientId'   // '283489330438468'      // '2915126152079198'
     APPLE_CLIENT_ID = 'my.deliverin.symplified.service',
 }
 
