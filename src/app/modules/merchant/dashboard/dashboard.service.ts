c479bebae18a601c9f48464425a3f716eaaef938
Wafi <ahmad.wafi@kalsym.com>
2022-11-09 11:57:41 +0800

    fix

--- src/app/modules/merchant/dashboard/dashboard.service.ts
+++ src/app/modules/merchant/dashboard/dashboard.service.ts
@@ -497,6 +497,17 @@ export class DashboardService
                 serviceType: serviceType
             }
         };
+
+        // Delete empty value
+        Object.keys(header.params).forEach(key => {
+            if (Array.isArray(header.params[key])) {
+                header.params[key] = header.params[key].filter(element => element !== null)
+            }
+            
+            if (!header.params[key] || (Array.isArray(header.params[key]) && header.params[key].length === 0)) {
+                delete header.params[key];
+            }
+        });
         
         return this._httpClient.get<{ totalSalesTotal: TotalSalesTotal[]; totalSalesDaily: TotalSalesDaily[]; 
             totalSalesWeekly: TotalSalesWeekly[]; totalSalesMonthly: TotalSalesMonthly[] }>
