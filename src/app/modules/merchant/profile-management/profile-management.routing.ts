b0978aecdf0161421f5dcbfa8fbe4d536e65a993
Wafi <ahmad.wafi@kalsym.com>
2023-03-28 15:59:56 +0800

    some fixes

--- src/app/modules/merchant/profile-management/profile-management.routing.ts
+++ src/app/modules/merchant/profile-management/profile-management.routing.ts
@@ -3,12 +3,22 @@ import { EditProfileComponent } from './edit-profile/edit-profile.component';
 import { GetClientResolver, GetProfilePaymentResolver } from './edit-profile/edit-profile.resolvers';
 
 export const profileManagementRoutes: Route[] = [
+
+    {
+        path     : '',
+        component: EditProfileComponent,
+        resolve  : {
+            clients: GetClientResolver,
+            profilePayment: GetProfilePaymentResolver
+        }
+    },
     {
         path     : ':panel-id',
         component: EditProfileComponent,
         resolve  : {
             clients: GetClientResolver,
             profilePayment: GetProfilePaymentResolver
-        },
+        }
     }
+
 ];
